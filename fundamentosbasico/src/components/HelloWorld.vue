74c1d92cc0c8ec5bd41344010f85ad332dbc75dd
GibranVasquez <158479829+GibranVasquez@users.noreply.github.com>
2025-03-23 16:51:21 -0600

    PaginaWeb-PorPartes

---
+++ fundamentosbasico/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Nav from './Nav.vue';
+import Carrusel from './Carrusel.vue';
+import Obje from './Obje.vue';
+import Cards from './Cards.vue';
+import Form from './Form.vue';
+import Carrusel2 from './Carrusel2.vue';
+
+</script>
+
+<template>
+  <h1>MOTOS DEPORTIVAS</h1>
+  <Nav />
+  <Carrusel />
+  <Obje />
+  <Cards />
+  <Form/>
+  <Carrusel2/>
+ </template>
+
+<style scoped>
+</style>
---
+++ fundamentosbasico/src/components/Obje.vue
@@ -0,0 +1,19 @@
+<template>
+            <div id="objetivo" class="mt-5">
+       
+
+       <div class="card col-md-12 text-center" style="background-color: lightblue; border: 2px solid #007bff;">
+           <div class="card-header" style="background-color: #1074df; color: white;">
+       <h2>Objetivo</h2>
+   </div>
+   <div class="card-body">
+       <p>
+           El objetivo de esta página es dar a conocer los distintos tipos de motos deportivas
+           disponibles y proporcionar información para quienes aún no comprenden el concepto de
+           una moto deportiva. Buscamos que más personas se interesen y se conviertan en verdaderos
+           fanáticos de estas impresionantes máquinas.
+       </p>
+   </div>
+</div>
+</div>
+</template>
