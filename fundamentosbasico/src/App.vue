06dc13e635de93c0e4e0190ee9328e296acb1d14
GibranVasquez <158479829+GibranVasquez@users.noreply.github.com>
2025-03-26 13:40:04 -0600

    Cambios de carpetas

--- fundamentosbasico/src/App.vue
+++ fundamentosbasico/src/App.vue
@@ -1,13 +1,8 @@
 <script setup>
- import HelloWorld from './components/HelloWorld.vue'
 
-import Padre from './components/Props/Padre.vue';
 </script>
 
 <template>
-<HelloWorld nombre="" />
-  <Padre/>
-
 
 </template>
 
