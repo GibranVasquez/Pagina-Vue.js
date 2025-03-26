eeb1920851ff454f4dcb1d261a193e3abfe7df98
GibranVasquez <158479829+GibranVasquez@users.noreply.github.com>
2025-03-26 14:42:06 -0600

    Eventos - Video 14

--- fundamentosbasico/src/components/Eventos/Eventos.vue
+++ fundamentosbasico/src/components/Eventos/Eventos.vue
@@ -1,17 +1,30 @@
 <template>
     <div class="container">
         <div class="row mt-4">
+
             <div>
-                <h4>Eventos Click</h4>
-                <button @click="clic" class="btn btn-danger" type="button">Dale clic</button>
-                <button @click="soyBtton" class="btn btn-warning mx-2" type="button">Dale clic 2</button>
-            </div>
-            <div>
-                <h4>Eventos Change</h4>
+                <h4 class="text-center">Eventos Change</h4>
                 <label class="input-group-text" for="">Ingresa tu nombre</label>
                 <input class="form-control mt-2" type="text" id="nombre" @change="nombreE">
                 <p>El nombre ingresado es:  <strong>{{ nombre }}</strong></p>
             </div>
+            
+            <div>
+
+            </div>
+            <h3 class="text-center">Evento de mousecenter y de mouseleave </h3>
+            <div  style="color: #ffff; width: 8000px; height: 300px; background-color: black; text-align: center; padding: 15px; border-radius: 14px; " @mousecenter="mostarMensaje" @mouseleave="ocultarMensaje">
+                <p>pase el mouse</p>
+                <p>{{ mensaje }}</p>
+
+            </div>
+
+            <div class="text-center">
+                <h4>Eventos Click</h4>
+                <button @click="clic" class="btn btn-danger" type="button">Dale clic</button>
+                <button @click="soyBtton" class="btn btn-warning mx-2" type="button">Dale clic 2</button>
+            </div>
+           
             
         </div>
     </div>
@@ -20,7 +33,8 @@
 export default {
     data(){
         return{
-            nombre:''
+            nombre:'',
+            mensaje:'',
         }
     },
     methods:{
@@ -32,6 +46,12 @@ export default {
         },
         nombreE(event){
             this.nombre =event.target.value
+        },
+        mostarMensaje(){
+            this.mensaje="El mouse esta dentro del rectangulo"
+        },
+        ocultarMensaje(){
+            this.mensaje= "El mouse esta afuera del rectangulo"
         }
     }
 }
